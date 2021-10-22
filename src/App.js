ba23bc34d664537b06656cfdb864f4b39d56c580
Ayten Abdullayeva <ayten.a@bestcomp.net>
2021-10-22 15:11:38 +0400

    start vabiss task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'devextreme/dist/css/dx.light.css';
+import { Button } from 'devextreme-react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+         <Button  type="success" stylingMode="outlined" >Devextreme Button</Button>
     </div>
   );
 }
