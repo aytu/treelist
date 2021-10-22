4c1ae50988e4ae39165b2a28a3b55b5feff4220a
Ayten Abdullayeva <ayten.a@bestcomp.net>
2021-10-22 15:59:46 +0400

    adding treelist to structure.js

---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const structures = [{
+    'id': 1,
+    'parent_id': -1,
+    'name': 'Field1',
+    'status': true
+}, {
+    'id': 2,
+    'parent_id': -1,
+    'name': 'Field2',
+    'status': true
+}, {
+    'id': 3,
+    'parent_id': 2,
+    'name': 'Field3',
+    'status': true
+}];
--- src/pages/structure.js
+++ src/pages/structure.js
@@ -1,9 +1,35 @@
+import { TreeList, Column, Editing } from 'devextreme-react/tree-list';
 import React from 'react'
+import { structures } from '../data'
 
 export default function Structure() {
     return (
         <div>
-            <h2>Structure</h2>
+              <TreeList
+                    id="structures"
+                    dataSource={structures}
+                    rootValue={-1}                  
+                    showRowLines={true}
+                    showBorders={true}
+                    columnAutoWidth={true}
+                    keyExpr="id"
+                    parentIdExpr="parent_id"
+                >
+                <Editing
+                        mode="row"   
+                        allowAdding={true}    
+                        allowUpdating={true}
+                        allowDeleting={true} />
+                 <Column dataField="name"
+                        dataType="string"
+                        width="60%"
+                        caption="Name" />
+                        
+                 <Column dataField="status"
+                        dataType="boolean"
+                        width="20%"
+                        caption="Status"/>
+             </TreeList>
         </div>
     )
 }
